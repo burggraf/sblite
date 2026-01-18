60971ed44860c329119844679755abc4cd9d5ed0
Mark Burggraf <markb@mantisbible.com>
2026-01-18 13:46:26 +0700

    .gitignore and restart.sh

--- test_apps/shoplite/restart.sh
+++ test_apps/shoplite/restart.sh
@@ -1,4 +1,4 @@
-cd ../../sblite && go build . && cp sblite test_apps/shoplite \
+cd ../.. && go build . && cp sblite test_apps/shoplite \
 && cd test_apps/shoplite \
 && ./sblite serve --db shoplite.db
 
